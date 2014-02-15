b1aea4b23508a1fe882b16b711561094a95c7f38
Alex Wollenschlaeger <alexw@panix.com>
2014-02-15 11:23:00 +0000

    added tests for new constructor and addNotes(), getNotes()

--- ContactImplTest.java
+++ ContactImplTest.java
@@ -1,25 +1,63 @@
 /**
  * JUnit 1.4 unit tests for ContactImpl class
  */
-
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ContactImplTest {
 
-	private ContactImpl contact;
+	@Test
+	public void getIdTest() {
 
-	@Before
-	public void setup() {
-		String name = "test name";
-		String notes = "test notes"
-		Contact contact = new ContactImpl(name, notes);
+		Contact zero = new ContactImpl("zero");
+		Contact one = new ContactImpl("one");
+		Contact two = new ContactImpl("two");
+
+		// make sure that IDs are unique and increment
+		int actual = two.getId() - zero.getId();
+		int expected = 2;
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void getIdTest() {
-		int actual = contact.getId();
-		int expected = 0;
+	public void getNameTest() {
+
+		String name = "Test";
+		Contact test = new ContactImpl(name);
+
+		String expected = name;
+		String actual = test.getName();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getNotesTest() {
+		String name = "Tony Est";
+		String notes = "Tony notes";
+
+		Contact test = new ContactImpl(name, notes);
+		String expected = notes;
+		String actual = test.getNotes();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getNotesNullTest() {
+		Contact test = new ContactImpl("Test");
+
+		String expected = "";
+		String actual = test.getNotes();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void addNotesTest() {
+		Contact test = new ContactImpl("test");
+		String notes = "some notes";
+		test.addNotes(notes);
+
+		String expected = notes;
+		String actual = test.getNotes();
 		assertEquals(expected, actual);
 	}
 
