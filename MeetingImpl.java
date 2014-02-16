60393830970ab97b31d087bf8ec8872aea2c8050
Alex Wollenschlaeger <alexw@panix.com>
2014-02-16 11:47:17 +0000

    implemented interface methods

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -8,19 +8,42 @@ import java.util.Set;
 */
 public interface Meeting {
 
+	private int Id;
+	private Calender date;
+	private Set<Contact> contacts;
+	private Contact user;
+
+	// counter to give out unique IDs
+	private static int count = 0;
+
+	public Meeting(Set<Contact> contacts, Calendar date) {
+		this.id = count;
+		count++;
+
+		this.contacts = new HashSet<Contact>();
+		this.contacts = contacts.clone();
+		this.contacts.add(this.user);
+
+		this.date = date;
+	}
+
 	/**
 	* Returns the id of the meeting
 	*
 	* @return the id of the meeting
 	*/
-	int getId();
+	public int getId() {
+		return this.id;
+	}
 	
 	/**
 	* Return the date of the meeting
 	*
 	* @return the date of the meeting
 	*/
-	Calendar getDate();
+	public Calendar getDate() {
+		return this.date;
+	}
 	
 	/**
 	* Return the details of people that attended the meeting
@@ -31,5 +54,10 @@ public interface Meeting {
 	*
 	* @return the details of people that attended the meeting.
 	*/
-	Set<Contact> getContacts();
+	public Set<Contact> getContacts() {
+		Set<Contact> attendees = new HashSet<Contact>();
+		attendees = this.contacts.clone();
+		attendees.remove(user);
+		return attendees;
+	}
 }
