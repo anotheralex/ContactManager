befc73145b3a6ad09a8cf656ca392153c1db9fd4
Alex Wollenschlaeger <alexw@panix.com>
2014-02-23 17:53:15 +0000

    implemented FutureMeeting interface. no methods or fields at this time

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -1,7 +1,7 @@
 /**
 * A meeting to be held in the future
 */
-public interface FutureMeeting extends Meeting{
+public class FutureMeetingImpl extends MeetingImpl {
 	// No methods here, this is just a naming interface
 	// (i.e. only necessary for type checking and/or downcasting)
 }
