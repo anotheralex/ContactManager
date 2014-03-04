b34664a1c20a1009b7a8dbe435ffffe6b93d7a0d
Alex Wollenschlaeger <alexw@panix.com>
2014-03-04 07:11:59 +0000

    addNewUser() test not working

--- ContactManagerImplTest.java
+++ ContactManagerImplTest.java
@@ -2,7 +2,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.io.*;
 
 /**
  * Tests for the ContactManagerImpl class
@@ -11,14 +11,53 @@ import java.util.GregorianCalendar;
 public class ContactManagerImplTest {
 
 	private ContactManager cm;
+	private static final String FILENAME = "contacts.txt";
 
 	@Before
 	public void setup() {
-		cm = new ContactManagerImpl();
-		String name = "Test name";
-		String notes = "Test notes";
-		cm.addNewContact(name, notes);
-		cm.flush();
+		/*
+		File file = new File(FILENAME);
+		try {
+			if (file.exists()) {
+				file.delete();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		*/
+
+		ContactManager cm = new ContactManagerImpl();
+
 	}
+
+	@Test(expected = NullPointerException.class)
+	public void addNewContactTestNullName() {
+		String name = null;
+		String notes = "Notes";
+		cm.addNewContact(name, notes);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void addNewContactTestNullNotes() {
+		String name = "Name";
+		String notes = null;
+		cm.addNewContact(name, notes);
+	}
+
+	@Test
+	public void getContactsById() {
+		cm.addNewContact("Name 1", "Notes");
+		cm.addNewContact("Name 2", "Notes");
+		cm.addNewContact("Name 3", "Notes");
+		cm.addNewContact("Name 4", "Notes");
+		
+		Set<Contact> contacts = new HashSet<>();
+		contacts = cm.getContacts(1, 2);
+
+		int expected = 2;
+		int actual = contacts.size();
+		assertEquals(expected, actual);
+	}
+
 
 }
