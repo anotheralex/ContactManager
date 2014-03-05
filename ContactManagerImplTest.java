0861e3d3d85ece379305fb3b6495113a353d0dcd
Alex Wollenschlaeger <alexw@panix.com>
2014-03-05 08:00:55 +0000

    tests for addContacts() by id or name

--- ContactManagerImplTest.java
+++ ContactManagerImplTest.java
@@ -47,19 +47,56 @@ public class ContactManagerImplTest {
 	}
 
 	@Test
-	public void addNewContactTestNormal() {
-		ContactManager cm = new ContactManagerImpl();
-		cm.addNewContact("name", "notes");
-	}
-
-	@Test
-	public void getContactsById() {
+	public void getContactsTestById() {
 		ContactManager cm = new ContactManagerImpl();
 		cm.addNewContact("name", "notes");
 		cm.addNewContact("name", "notes");
 		
 		Set<Contact> contacts = new HashSet<>();
+		
+		contacts = cm.getContacts(0);
+		int expected = 1;
+		int actual = contacts.size();
+		assertEquals(expected, actual);
+		
 		contacts = cm.getContacts(0, 1);
+		expected = 2;
+		actual = contacts.size();
+		assertEquals(expected, actual);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void getContactsTestUnknownId() {
+		ContactManager cm = new ContactManagerImpl();
+		cm.addNewContact("name", "notes");
+		cm.addNewContact("name", "notes");
+		
+		Set<Contact> contacts = new HashSet<>();
+		
+		contacts = cm.getContacts(3);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void getContactsTestNullName() {
+		ContactManager cm = new ContactManagerImpl();
+		cm.addNewContact("name", "notes");
+		cm.addNewContact("name", "notes");
+		cm.addNewContact("nam1", "notes");
+		
+		Set<Contact> contacts = new HashSet<>();
+		String name = null;
+		contacts = cm.getContacts(name);
+	}
+
+	@Test
+	public void getContactsTestByName() {
+		ContactManager cm = new ContactManagerImpl();
+		cm.addNewContact("name", "notes");
+		cm.addNewContact("name", "notes");
+		cm.addNewContact("nam1", "notes");
+		
+		Set<Contact> contacts = new HashSet<>();
+		contacts = cm.getContacts("name");
 
 		int expected = 2;
 		int actual = contacts.size();
