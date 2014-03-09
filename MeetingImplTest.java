6c075db5dc1986c13aabe60a49f738663af6b5b8
Alex Wollenschlaeger <alexw@panix.com>
2014-03-09 14:44:58 +0000

    tidied up

--- MeetingImplTest.java
+++ MeetingImplTest.java
@@ -2,7 +2,6 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 /**
  * Tests for the MeetingImpl class
@@ -31,26 +30,7 @@ public class MeetingImplTest {
 
 	@Test
 	public void getIdTest() {
-
-		/*
-		//Calendar date = new GregorianCalendar(2013, 01, 01);
-		date = Calender.getInstance();
-
-		Contact contact1 = new ContactImpl(1, "Contact1");
-		Contact contact2 = new ContactImpl(2, "Contact2");
-
-		Set<Contact> contacts1 = new HashSet<Contact>();
-		contacts1.add(contact1);
-		*/
-
 		Meeting m = new MeetingImpl(1, contacts, date);
-
-		/*
-		Set<Contact> contacts2 = new HashSet<Contact>();
-		contacts2.add(contact2);
-
-		Meeting meeting2 = new MeetingImpl(contacts2, date);
-		*/
 
 		int expected = 1;
 		int actual = m.getId();
@@ -67,26 +47,11 @@ public class MeetingImplTest {
 
 	@Test
 	public void getContactsTest() {
-
-		/*
-		Calendar date = new GregorianCalendar(2013, 01, 01);
-
-		Contact contact1 = new ContactImpl("Contact1");
-		Contact contact2 = new ContactImpl("Contact2");
-
-		Set<Contact> contacts1 = new HashSet<Contact>();
-		contacts1.add(contact1);
-		contacts1.add(contact2);
-		*/
-
 		Meeting m = new MeetingImpl(0, contacts, date);
 
 		Set<Contact> actual = m.getContacts();
-
 		Set<Contact> expected = contacts;
 
 		assertEquals(expected, actual);
-
 	}
-
 }
